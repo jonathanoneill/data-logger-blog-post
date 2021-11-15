b60f99e99f436ff192da4b59bbb58ba2c7a02f98
Jonathan O'Neill <jonathan.p.oneill@gmail.com>
2021-11-15 19:00:59 +0000

    Data Logger - Part 3 - Visualization

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Code for blog post to build a simple data logger system for temperature and rela
 
 [Part 1 - Hardware](https://jonathanoneill.net/2020/10/31/data-logger-part-1-hardware/) - build a data logger for recording temperature and humidity using a Raspberry Pi
 
-Part 2 - Storage - send data to an AWS Timestream database
+[Part 2 - Storage](https://jonathanoneill.net/2021/11/15/data-logger-part-2-storage/) - send data to an AWS Timestream database
 
-Part 3 - Visualization - viewing your data in a graphical form
+[Part 3 - Visualization](https://jonathanoneill.net/2021/11/15/data-logger-part-3-visualization/) - viewing your data in a graphical form
 
  This post is based on the Raspberry Pi Tutorial [Raspberry Pi: Measure Humidity and Temperature with DHT11/DHT22](https://tutorials-raspberrypi.com/raspberry-pi-measure-humidity-temperature-dht11-dht22/)
---
+++ data_plotting.py
@@ -0,0 +1,49 @@
+import awswrangler as wr
+
+from matplotlib import pyplot as plt
+
+DATABASE_NAME = "DataLoggerDB"
+TABLE_NAME = "IoT"
+
+QUERY_DATE = "2021-11-15"
+QUERY_SENSOR = "001"
+
+# Get date from Timestream database
+SELECT_SQL = f"SELECT * FROM {DATABASE_NAME}.{TABLE_NAME} WHERE SensorId = '{QUERY_SENSOR}' AND DATE(time) = '{QUERY_DATE}' ORDER BY time ASC"
+
+df = wr.timestream.query(SELECT_SQL)
+
+print('Raw data from Timestream')
+print(df)
+
+# Data cleaning - Remove date and seconds
+df['time'] = df['time'].apply(lambda t: t.strftime('%H:%M'))
+
+# Data cleaning - Pivot table so Temperature and Relative Humidity are columns
+df = df.pivot(index="time", columns="measure_name", values="measure_value::double")
+df.reset_index(inplace=True)
+
+# Data cleaning - Remove rows where there null values
+df.dropna()
+
+# Data cleaning - Smooth out data before plotting
+df['Temperature'] = df['Temperature'].interpolate(method='polynomial', order=2)
+df['Relative Humidity'] = df['Relative Humidity'].interpolate(method='polynomial', order=2)
+
+print('Cleaned data for plotting')
+print(df)
+
+# Plot data
+figure, axes = plt.subplots(2)
+
+figure.suptitle(f'Temperature and Relaive Humidity\n({QUERY_SENSOR} - {QUERY_DATE})')
+
+df.plot(kind='line',x='time',y='Temperature', ax=axes[0], color='tab:blue', legend=None)
+df.plot(kind='line',x='time',y='Relative Humidity', ax=axes[1], color='tab:orange', legend=None)
+
+axes[0].set_ylabel('Temperature ($^\circ$C)')
+axes[0].set_xlabel(None)
+axes[1].set_ylabel('Relavive Humidity (%)')
+axes[1].set_xlabel('Time')
+
+plt.show()
